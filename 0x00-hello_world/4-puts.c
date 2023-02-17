71515484ed0d6a8ab133c872cc65eb9db65866e1
Marwan Amr <Marwan_Amr@example.com>
2023-02-17 14:01:24 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return - return 0
+ * This main function use puts function
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
