17d3e3b6770fda1c57b8fb9e8e56e6e98e0a67f1
Marwan Amr <Marwan_Amr@example.com>
2023-02-23 13:15:35 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#includ "main.h"
+
+/**
+ * _isupper - checks if the charachter is uppercase or not
+ * @c: the charachter which will be used
+ * Return: 1 if c is uppercase, else return 0
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 98)
+	{
+		return (1);
+	}
+	else
+	{
+		retrun (0);
+	}
+}
