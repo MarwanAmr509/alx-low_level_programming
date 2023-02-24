1cca8417795a9458168616cb53137facefcb90d3
Marwan Amr <Marwan_Amr@example.com>
2023-02-24 10:06:02 -0800

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print charachters from a to z
+ * Return: alphabets
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
