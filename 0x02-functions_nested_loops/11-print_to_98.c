65d5c50926d8165646b901f0dd029d0da803d57e
Marwan Amr <Marwan_Amr@example.com>
2023-02-24 12:43:13 -0800

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,34 +9,16 @@
 
 void print_to_98(int n)
 {
-	int x, y;
-
-	if (n <= 98)
+	while (n <98)
 	{
-		for (x = n; x <= 98; x++)
-		{
-			if (x != 98)
-			{
-				printf("%d, ", x);
-			}
-			else if (x == 98)
-			{
-				printf("%d\n", x);
-			}
-		}
+		printf("%i, ",n);
+		n++;
 	}
-	else if (n >= 98)
+	while (n > 98)
 	{
-		for (y = n; y <= 98; y++)
-		{
-			if (y != 98)
-			{
-				printf("%d, ", y);
-			}
-			else if (y == 98)
-			{
-				printf("%d\n", y);
-			}
-		}
+		printf("%i, ",n);
+		n--;
 	}
+	printf("98");
+	putchar('\n');
 }
