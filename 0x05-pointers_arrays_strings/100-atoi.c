f54701b1d31a1c435f384826bcdbdba825367a64
Marwan Amr <Marwan_Amr@example.com>
2023-03-01 05:10:24 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: input string
+ * Return: the integer which come from the string
+ */
+
+int _atoi(char *s)
+{
+	int num = 0;
+	int n = 0;
+	int len = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len && k == 0)
+	{
+		if (s[i] == '-')
+		{
+			++j;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = s[i] - '0';
+			if (j % 2)
+			{
+				digit = -digit;
+			}
+			n = n * 10 + digit;
+			k = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			f = 0;
+		}
+		i++;
+	}
+	if (k == 0)
+	{
+		return (0);
+	}
+	return (n);
+}
