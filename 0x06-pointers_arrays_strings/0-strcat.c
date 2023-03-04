c3a8a8ca689077d9cbbb4f91049894a204b610e2
Marwan Amr <Marwan_Amr@example.com>
2023-03-04 13:12:06 -0800

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strcat - Concatenates the string pointed to by @src, including the x
+ * *_strcat - Concatenates the string pointed to by @src, including the x
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest
