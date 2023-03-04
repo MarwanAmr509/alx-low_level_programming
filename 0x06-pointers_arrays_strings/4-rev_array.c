c264c9a88059a5c179f704e39ffba3792b84155c
Marwan Amr <Marwan_Amr@example.com>
2023-03-04 13:43:53 -0800

    more pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: the input array
+ * @n: number of elements of the array 
+ * Return: 1 if c is digit, else return 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = n - 1; i > n / 2; i--)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
+	}
+}
