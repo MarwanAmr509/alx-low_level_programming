52b7ca5d50c46c2f250cd60c6ae8fb4c26917d0b
Marwan Amr <Marwan_Amr@example.com>
2023-03-04 13:29:19 -0800

    more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int len = 0;
 
-	while (dest[i++])
+	while (src[i++])
 	{
 		len++;
 	}
 	for (i = 0; src[i] && i < n; i++)
 	{
-		dest[len++] = src[i];
+		dest[i] = src[i];
 	}
 	for (i = len; i < n; i++)
 	{
