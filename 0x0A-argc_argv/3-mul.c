122b6fde0a32b0f7543bd5e7d9484b9b114dd80c
Marwan Amr <Marwan_Amr@example.com>
2023-03-10 08:06:48 -0800

    argv-argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: count the number of argumnets
+ * @argv: the arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 0)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
